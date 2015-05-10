4d1ada2d07b7f505b82c78d671821177345d3828
Kayla <kayla.comalli@gmail.com>
2015-05-10 16:14:18 -0400

    initial commit

---
+++ add-content.js
@@ -0,0 +1,18 @@
+// this example adds a greeting into an HTML page.
+// The greeting changes depending on the time of day.
+
+var today = new Date();
+vare hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
